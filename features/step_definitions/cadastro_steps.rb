75b4861323168b11a6616fb4fd427f34ff5f82a2
Anderson Ferreira <andersonfoliveira@yahoo.com>
2021-01-06 23:19:09 -0300

    implementando validação dos steps

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,10 +1,13 @@
 Dado('que acesso a página de cadastro:') do
-    visit 'http://parodify.qaninja.com.br/'
+    visit 'http://parodify.qaninja.com.br'
+    click_on'EXPERIMENTE AGORA'
 end
   
 Quando('submeto o meu cadastro com:') do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
+   user = table.rows_hash
+   find('input[placeholder="Seu melhor email"]').set user[:email] 
+   find('[id="user_password"]').set user[:senha]
+   find('[id="user_password_confirmation"]').set user[:senha_confirma]
 end
   
 Então('devo ser redirecionado para a área logada') do
