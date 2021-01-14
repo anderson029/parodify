5c4acfc8d6622557fef2692cfece55fff1ed4e2a
Anderson Ferreira <andersonfoliveira@yahoo.com>
2021-01-13 23:00:44 -0300

    finalizando validação do step

--- features/step_definitions/reproduzir-parodia.rb
+++ features/step_definitions/reproduzir-parodia.rb
@@ -13,14 +13,10 @@ end
 
 Quando("toco a seguinte canção:") do |table|
   @parody = table.rows_hash
-
   find("a", text: @parody[:banda]).click
-
-  song = find(".song-item", text: @parody[:parodia])
-  song.find(".fa-play-circle").click
+  find("[class='fas fa-play-circle fa-2x']").click
 end
 
 Então("essa paródia deve ficar em modo de reprodução") do
-  song_play = find("playing")
-  expect(song_play).to have_text @parody[:parodia]
+  expect(page.assert_all_of_selectors("i[class='fas fa-2x fa-pause-circle']")).to include ("i[class='fas fa-2x fa-pause-circle']")
 end
